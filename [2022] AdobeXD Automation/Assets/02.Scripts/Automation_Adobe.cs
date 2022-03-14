9954362d45a60f22f98440e4e41b0c81f2da9273
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2022-03-14 23:22:13 +0900

    automation window 초안 다듬기
    - EditorWindow 초기 형태 설정함.
    - 디버깅 상태로 병합메서드 즉시 실행하도록 설정
    - 라벨코드 원형으로 나열해둠

--- [2022] AdobeXD Automation/Assets/02.Scripts/Automation_Adobe.cs
+++ [2022] AdobeXD Automation/Assets/02.Scripts/Automation_Adobe.cs
@@ -2,12 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 public class Automation_Adobe : EditorWindow
 {
+    public enum SplitString
+	{
+        Underbar,
+        Test2
+	}
+
     Canvas m_rootCanvas;
     GameObject m_target;
 
+    string m_split;
+    string m_labelButton;
+    string m_labelBoundary;
+    string m_labelBackground;
+    string m_labelText;
+    string m_labelImage;
+    //SplitString m_split;
 
     string myString = "Hello World";
     bool groupEnabled;
@@ -29,59 +43,50 @@ public class Automation_Adobe : EditorWindow
 	private void OnGUI()
 	{
         GUILayout.Label("Base settings", EditorStyles.boldLabel);
-
-
         // 마지막 인자를 true로 두어야 Hierarchy에서 객체를 가져올 수 있다.
-        m_target = (GameObject)EditorGUILayout.ObjectField("Hello", m_target, typeof(GameObject), true);
-        myString = EditorGUILayout.TextField("text field", myString);
+        m_rootCanvas = (Canvas)EditorGUILayout.ObjectField("root canvas", m_rootCanvas, typeof(Canvas), true);
+        m_target = (GameObject)EditorGUILayout.ObjectField("target object", m_target, typeof(GameObject), true);
 
-        //EditorGUILayout.BeginFadeGroup(1);
-        //EditorGUILayout.EndFadeGroup();
+        EditorGUILayout.Space();
+        GUILayout.Label("정의된 문자코드", EditorStyles.boldLabel);
+        m_split = EditorGUILayout.TextField("split string", m_split);
+        GUILayout.Label("Labels", EditorStyles.boldLabel);
+        m_labelButton = EditorGUILayout.TextField("button", m_labelButton);
+        m_labelBoundary = EditorGUILayout.TextField("boundary", m_labelBoundary);
+        m_labelBackground = EditorGUILayout.TextField("background", m_labelBackground);
+        m_labelText = EditorGUILayout.TextField("text", m_labelText);
+        m_labelImage = EditorGUILayout.TextField("image", m_labelImage);
 
-        EditorGUI.BeginDisabledGroup(true);
-        EditorGUILayout.HelpBox("Hello group", MessageType.Info);
-        EditorGUI.EndDisabledGroup();
 
-        //groupEnabled = IsCanOperate(m_rootCanvas, m_target);
+        //m_split = (SplitString)EditorGUILayout.EnumFlagsField("split code", m_split);
+        //myString = EditorGUILayout.TextField("text field", myString);
 
-        //groupEnabled = EditorGUILayout.BeginToggleGroup("Operatable", IsCanOperate(m_rootCanvas, m_target));
-        //if(groupEnabled)
-		//{
-        //    EditorGUILayout.HelpBox("Hello", MessageType.Info);
-		//}
-        //else
-		//{
-        //
-		//}
-        //EditorGUILayout.EndToggleGroup();
+        bool operatable = IsCanOperate(m_rootCanvas, m_target);
 
-        //EditorGUILayout.BeginToggleGroup("Operatable", groupEnabled);
-
-        //EditorGUILayout.EndToggleGroup();
-
-        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-
-        myBool = EditorGUILayout.Toggle("Toggle", myBool);
-        if(myBool)
+        if(operatable)
 		{
-            EditorGUI.BeginDisabledGroup(myBool);
-            EditorGUILayout.HelpBox("true", MessageType.Info);
-            EditorGUI.EndDisabledGroup();
+            EditorGUILayout.HelpBox("변환 준비 완료", MessageType.Info);
+        }
+        else
+		{
+            EditorGUILayout.HelpBox("root canvas와 root object를 할당하세요", MessageType.Warning);
 		}
 
-        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
-        EditorGUILayout.EndToggleGroup();
-
-        if(GUILayout.Button(doButton))
-		{
+        EditorGUI.BeginDisabledGroup(!operatable);
+        if (GUILayout.Button(doButton))
+        {
             Debug.Log("do Button");
 
-            if(EditorUtility.DisplayDialog("Title :: 생성 확인창", "Message : 객체를 생성하시겠습니까?", "OK", "Cancel"))
-			{
-                PrintObjects(m_target);
+            // 디버깅
+            PrintObjects(m_target);
 
-			}
-		}
+            // 디버깅 끝나면 활성화
+            //if (EditorUtility.DisplayDialog("Title :: 생성 확인창", "Message : 객체를 생성하시겠습니까?", "OK", "Cancel"))
+            //{
+            //    PrintObjects(m_target);
+            //}
+        }
+        EditorGUI.EndDisabledGroup();
 	}
 
     private bool IsCanOperate(Canvas _rCanvas, GameObject _target)
@@ -104,10 +109,23 @@ public class Automation_Adobe : EditorWindow
             return;
 		}
 
-        
+        List<Transform> trs = _obj.transform.GetComponentsInChildren<Transform>().ToList();
+
+        trs.ForEach(x =>
+        {
+            if (x.name.Contains(m_labelButton))
+            {
+                Debug.Log($"find btn label : name {x.name}");
+            }
+        });
 
         Debug.Log(_obj.name);
 
+
+
+        // 라벨 문자 확인 끝나고 생성단계 진행
+        return;
+        
         GameObject obj = new GameObject("Element 1");
 
         obj.transform.SetParent(_obj.transform);
