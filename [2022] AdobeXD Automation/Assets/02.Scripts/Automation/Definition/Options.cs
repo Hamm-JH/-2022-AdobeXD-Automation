869015066af73d119d4c64e5161756942a4fd473
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2022-03-25 21:17:23 +0900

    Define styles (enum)

---
+++ [2022] AdobeXD Automation/Assets/02.Scripts/Automation/Definition/Options.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Automation.Definition
+{
+	public enum Styles
+	{
+		Default_UI,
+		TextmeshPro,
+	}
+}
